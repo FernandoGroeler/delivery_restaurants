358273ae3e1084b5b672a06af08d664bc347859e
fernando.groeler <fernando.groeler@gmail.com>
2017-11-30 21:25:42 -0200

    cors.

--- src/main/java/delivery/rest/RestaurantsController.java
+++ src/main/java/delivery/rest/RestaurantsController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class RestaurantsController {
     @Autowired
     RestaurantsRepository restaurantsRepository;
--- src/main/java/delivery/rest/RestaurantsItemsController.java
+++ src/main/java/delivery/rest/RestaurantsItemsController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class RestaurantsItemsController {
     @Autowired
     RestaurantsItemsRepository restaurantsItemsRepository;
